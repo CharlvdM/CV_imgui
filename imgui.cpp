56af85b3f193855b6ebfe1e2d43f4bbb6c0370b2
Charl van de Merwe <charl.vandemerwe@transnet.net>
2023-04-05 20:33:16 +0200

    Suppressed an additional warning

--- imgui.cpp
+++ imgui.cpp
@@ -881,6 +881,7 @@ CODE
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #pragma GCC diagnostic ignored "-Wredundant-decls"
+#pragma GCC diagnostic ignored "-Wfloat-equal"
 #endif
 
 //-------------------------------------------------------------------------
